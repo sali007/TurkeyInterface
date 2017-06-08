f9317427d8bcdb398b49c8decedbab9c070af83d
nikfed1 <nikfed1@gmail.com>
2017-06-08 20:11:41 +0300

    new for remote

--- site/config/F1/-100001122.js
+++ site/config/F1/-100001122.js
@@ -3,10 +3,10 @@ UIGroup=[
 {"__type":"provider","id":"413","tag":"visible","logo":"413.gif","sName":"Wallet one","small_logo":""},
 //{"__type":"provider","id":"384","tag":"visible","logo":"384.gif","sName":"Мобильные деньги","small_logo":""},
 {"__type":"provider","id":"1949","tag":"visible","logo":"1949.gif","sName":"Деньги@Mail.Ru","small_logo":""},
-{ "__type": "provider", "id": "56", "tag": "visible", "logo": "56.gif", "sName": "Webmoney", "small_logo": "" },
+//{ "__type": "provider", "id": "56", "tag": "visible", "logo": "56.gif", "sName": "Webmoney", "small_logo": "" },
 { "__type": "provider", "id": "400013", "tag": "visible", "logo": "400013.gif", "sName": "RBK Money", "small_logo": "" },
 
-{ "__type": "provider", "id": "300003", "tag": "visible", "logo": "300003.gif", "sName": "Яндекс Деньги", "small_logo": "" },
+//{ "__type": "provider", "id": "300003", "tag": "visible", "logo": "300003.gif", "sName": "Яндекс Деньги", "small_logo": "" },
 //{ "__type": "provider", "id": "300004", "tag": "visible", "logo": "300004.gif", "sName": "Ukash", "small_logo": "" },
 { "__type": "provider", "id": "300005", "tag": "visible", "logo": "300005.gif", "sName": "Монета.ru", "small_logo": "" }
 
